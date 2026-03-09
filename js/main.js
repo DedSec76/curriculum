68309f2ff34646652e7a0a0fafac71af77f96d79
Aldair Rutte <xxpiltisxx@gmail.com>
2026-03-09 01:46:44 -0500

    arreglando puntaje en lighthouse

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-import { toggleMenu, imagesCarousel, scrollToSection, validateForm } from './ui.mjs';
+import { toggleMenu, imagesCarousel, scrollToSection, validateForm, renderListWithTemplate } from './ui.mjs';
+
+const data = await fetch("data/projects.json")
+const projects = await data.json()
+
+const parentElement = document.querySelector(".projects__list")
+console.log(parentElement)
+
+renderListWithTemplate({
+                        parentElement: parentElement, 
+                        list: projects
+})
 
 document.addEventListener('DOMContentLoaded', () => {
     toggleMenu();
